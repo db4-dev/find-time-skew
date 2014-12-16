3557a56b38a4e283535854c7dc0ccbaf9105b110
db4-dev <db4dev@cassault.com>
2014-12-16 09:48:59 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 // Main file
 
-console.log("Hello from Travis");
+var art = require('ascii-art'),
+    ntpClient = require('ntp-client');
 
-var art = require('ascii-art'); 
-
+/*
 art.font('Demo!', 'Doom', function(rendered){ 
     console.log(art.style(rendered, 'blue_bg+red+blink+inverse')); 
 }); 
 
 
-var ntpClient = require('ntp-client');
 
 ntpClient.getNetworkTime("pool.ntp.org", 123, function(err, date) {
     if(err) {
@@ -19,4 +18,20 @@ ntpClient.getNetworkTime("pool.ntp.org", 123, function(err, date) {
 
     console.log("Current time : ");
     console.log(date);
+});
+
+*/
+
+ntpClient.getNetworkTime("pool.ntp.org", 123, function(err, date) {
+    if(err) {
+        console.error(err);
+        return;
+    }
+
+    var sysdate = new Date();
+  
+    console.log("Current time : ", date);
+    console.log("System Time : ", sysdate);
+  
+    console.log("Skew: ", (sysdate - date));
 });
