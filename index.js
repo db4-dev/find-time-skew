7883d5be63d75a10718b47550b18bfa01fcfd685
db4-dev <db4dev@cassault.com>
2014-12-16 10:11:22 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -3,25 +3,6 @@
 var art = require('ascii-art'),
     ntpClient = require('ntp-client');
 
-/*
-art.font('Demo!', 'Doom', function(rendered){ 
-    console.log(art.style(rendered, 'blue_bg+red+blink+inverse')); 
-}); 
-
-
-
-ntpClient.getNetworkTime("pool.ntp.org", 123, function(err, date) {
-    if(err) {
-        console.error(err);
-        return;
-    }
-
-    console.log("Current time : ");
-    console.log(date);
-});
-
-*/
-
 function pad(i) {
   if (i < 10) {
     return "0" + i;
