3737611783bf482aba4c3901941592d6b658eced
db4-dev <db4dev@cassault.com>
2014-12-16 10:09:46 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function pad(i) {
 }
 
 function formatDate(d) {
-  return pad(d.getDate()) + "." + pad(d.getMonth() + 1) + "." + d.getFullYear() + " " + pad(d.getHours()) + ":" + pad(d.getMinutes()) + ":" + pad(d.getSeconds());
+  return pad(d.getDate()) + "." + pad(d.getMonth() + 1) + "." + d.getFullYear() + " - " + pad(d.getHours()) + ":" + pad(d.getMinutes()) + ":" + pad(d.getSeconds());
 }
 
 ntpClient.getNetworkTime("pool.ntp.org", 123, function(err, date) {
@@ -45,9 +45,14 @@ ntpClient.getNetworkTime("pool.ntp.org", 123, function(err, date) {
     console.log("Current time : ", date);
     console.log("System Time : ", sysdate);
   
-    console.log("Skew: ", (sysdate - date));
+    console.log("Skew: ", Math.abs(sysdate - date));
   
   art.font(formatDate(date), 'Doom', function(rendered) {
-    console.log(rendered);
+    if (Math.abs(sysdate - date) < 1 * 60 * 1000) { // If skew is bigger than 1000
+      console.log(art.style(rendered, 'blue_bg+red'));
+    }
+    else {
+      console.log(rendered);
+    }
   });
 });
